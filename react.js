965dcd2aa48436929b6062f6c877b28a669aaaf6
Morgan <mrgnmcsmith@gmail.com>
2018-03-14 22:37:57 -0400

    Dice selection reflected in dice options

--- react.js
+++ react.js
@@ -1,18 +1,27 @@
 class Dice extends React.Component {
+
+    diceClick= () => {
+        alert(this.props.selected);
+        this.props.selected = this.props.selected == 0 ? 1 : 0;
+        this.props.onSelect(this.props.sides);
+    }
+
     render() {
         var className = "dice";
         if (this.props.selected) {
             className += " selected";
+            alert("select");
         }
         return (
-            <div className={className}>
+            <div className={className} onClick = {this.diceClick}>
             {this.props.sides}
             </div>
         );
     }
 }
 
-class DiceOptions extends React.Component {
+class RollOptions extends React.Component {
+
     render() {
         var options = [];
         for (var i = 1; i < 9 ; i++) {
@@ -25,6 +34,7 @@ class DiceOptions extends React.Component {
                 <select>
                     {options}
                 </select>
+                <div> {this.props.selected} </div>
                 <input type="number"/>
             </div>
         );
@@ -32,6 +42,15 @@ class DiceOptions extends React.Component {
 }
 
 class DiceDrawer extends React.Component {
+    constructor(){
+        super();
+        this.state = {selectedDice: 0};
+    }
+    handleSelection = (diceValue) => {
+        alert("Dice value " + diceValue);
+       this.setState({selectedDice: diceValue});
+    }
+
     render() {
         var diceSides = [4, 6, 10, 20];
         var diceOptions = [];
@@ -39,14 +58,14 @@ class DiceDrawer extends React.Component {
             var isSelected = 0;
             var sides = diceSides[i];
             diceOptions.push(
-                <Dice sides={sides} selected={isSelected} key={i} />
+                <Dice sides={sides} selected={isSelected} key={i} onSelect={this.handleSelection}/>
             );
         }
 
         return (
             <div>
             <div className="diceDrawer">{diceOptions}</div>
-            <DiceOptions/>
+            <RollOptions selected = {this.state.selectedDice}/>
             </div>
         );
     }
